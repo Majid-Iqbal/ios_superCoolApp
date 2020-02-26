f0c934d1507132aebcdf97abae78fa3b3bfc4f24
“majid” <majid.iqbal54@gmail.com>
2020-02-26 13:17:04 +0500

    modified

--- SuperCoolApp/ViewController.swift
+++ SuperCoolApp/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var str = "Hello World"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(str)
     }
 
 
