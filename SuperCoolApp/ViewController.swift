948333d7373066fe35ae784f0534ccb88f142707
“majid” <majid.iqbal54@gmail.com>
2020-02-26 13:13:15 +0500

    Initial Commit

---
+++ SuperCoolApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperCoolApp
+//
+//  Created by Majid on 26/02/2020.
+//  Copyright © 2020 Majid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
